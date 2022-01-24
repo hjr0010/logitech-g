df85d82899d677116340ff38a04b465c8744daa7
Hunter <hjr0010@mix.wvu.edu>
2022-01-24 12:34:21 -0500

    V-A.1 Top Navbars added in (Hamburger dropdown, cart and searchbar need functionality.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { CarouselComponent } from './mainitems/carousel.component';
 import { LinkBarComponent } from './navbars/linkbar.component';
 import { MessageBarComponent } from './navbars/messagebar.component';
 import { NavBarComponent } from './navbars/navbar.component';
@@ -11,7 +12,8 @@ import { NavBarComponent } from './navbars/navbar.component';
     AppComponent,
     LinkBarComponent,
     MessageBarComponent,
-    NavBarComponent
+    NavBarComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule
