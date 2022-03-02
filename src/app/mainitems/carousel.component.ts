ffb6c0a9359f0c530efd75f20d87605328188dc8
BuildTools <unconfigured@null.spigotmc.org>
2022-03-02 00:27:34 -0500

    Full Completed Navbar, needs alignment

--- src/app/mainitems/carousel.component.ts
+++ src/app/mainitems/carousel.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'logitech-carousel',
-    templateUrl: 'carousel.component.html'
+    templateUrl: 'carousel.component.html',
+    styleUrls: ['carousel.component.css']
 })
 export class CarouselComponent {
 
--- src/app/navbars/messagebar.component.ts
+++ src/app/navbars/messagebar.component.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'logitech-messagebar',
     templateUrl: 'messagebar.component.html',
+    styleUrls: ['messagebar.component.css']
 })
 export class MessageBarComponent {
 }
