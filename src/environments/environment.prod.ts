c260a0128fb369bf82a4a2ff309438a6cafcb7b7
BuildTools <unconfigured@null.spigotmc.org>
2022-03-19 17:34:28 -0400

    Firebase hosting working and rearranged homepage carousel titles

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,26 @@
 export const environment = {
   production: true
 };
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBwZp0em5qEfQjgW2Zd6KnhHu106XpwHWI",
+  authDomain: "logitechg-dce95.firebaseapp.com",
+  databaseURL: "https://logitechg-dce95-default-rtdb.firebaseio.com",
+  projectId: "logitechg-dce95",
+  storageBucket: "logitechg-dce95.appspot.com",
+  messagingSenderId: "1085130157984",
+  appId: "1:1085130157984:web:34f360fa88ed68173dbec5",
+  measurementId: "G-JXTDRLX8JL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,25 @@ export const environment = {
   production: false
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBwZp0em5qEfQjgW2Zd6KnhHu106XpwHWI",
+  authDomain: "logitechg-dce95.firebaseapp.com",
+  databaseURL: "https://logitechg-dce95-default-rtdb.firebaseio.com",
+  projectId: "logitechg-dce95",
+  storageBucket: "logitechg-dce95.appspot.com",
+  messagingSenderId: "1085130157984",
+  appId: "1:1085130157984:web:34f360fa88ed68173dbec5",
+  measurementId: "G-JXTDRLX8JL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
