8ed97b2e6c288fe7f6fc038e81a21e055dfebd90
Hunter <hjr0010@mix.wvu.edu>
2022-04-06 14:53:43 -0400

    Fixed sending signup info, need to finish rest of v0.10

--- src/app/auth/authentication/authentication.component.ts
+++ src/app/auth/authentication/authentication.component.ts
@@ -17,7 +17,7 @@ export class AuthenticationComponent implements OnInit {
   onSubmit(data:NgForm) {
     console.log("Button clicked");
     console.log(data.value);
-    data.reset();
+
 
     this.auth.signup(data.value.email, data.value.password).subscribe(
       data => {
@@ -27,5 +27,7 @@ export class AuthenticationComponent implements OnInit {
         console.log(error);
       }
     )
+
+    data.reset();
   }
 }
